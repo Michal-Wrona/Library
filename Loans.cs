5791d6e0df5ea5a0692724e13faff069ec916a46
Michal-Wrona <michalwro112@gmail.com>
2021-11-06 10:49:15 +0100

    sprawdzenie gita po formacie

--- Loans.cs
+++ Loans.cs
@@ -14,24 +14,24 @@ namespace Bibkioteka1
             _books1Loans = new List<Books1> ();
         }
 
-        public Books1 BorrowBook(int id)
-        {
-            int ID = id;
-            Console.WriteLine("Podaj id");
-            Books1 borrowBook = new Books1();
+        //public Books1 BorrowBook(int id)
+        //{
+        //    int ID = id;
+        //    Console.WriteLine("Podaj id");
+        //    Books1 borrowBook = new Books1();
 
-            //var idi = _books1.Select(x => x.id);
+        //    //var idi = _books1.Select(x => x.id);
 
-            /* var idi = from book in _books1 where book.id == ID select book;*/ // to jest poprawne znalezienie obiektu book z listy _books1?
+        //    /* var idi = from book in _books1 where book.id == ID select book;*/ // to jest poprawne znalezienie obiektu book z listy _books1?
 
-            //var elementPos = array.map(function(x) { return x.id; }).indexOf(idYourAreLookingFor);
-            //var objectFound = array[elementPos];
+        //    //var elementPos = array.map(function(x) { return x.id; }).indexOf(idYourAreLookingFor);
+        //    //var objectFound = array[elementPos];
 
-            foreach (Books1 book in _books1)
-            {
-                if (_books1.
-            }
+        //    foreach (Books1 book in _books1)
+        //    {
+        //        if (_books1.
+        //    }
 
-        }
+        
     }
 }
